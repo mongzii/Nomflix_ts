df5e4fd6c1f849c548ec4b7e694e31c250a2870f
moonjiyoung <umarg228@naver.com>
2023-09-12 14:11:38 +0900

    feat: header search완료

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
 import { Link, useMatch } from "react-router-dom";
+import { useState } from "react";
 
 const Nav = styled.nav`
   display: flex;
@@ -50,6 +51,9 @@ const Item = styled.li`
 `;
 const Search = styled.span`
   color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
   svg {
     height: 25px;
   }
@@ -65,6 +69,11 @@ const Circle = styled(motion.span)`
   margin: 0 auto;
   background-color: ${props => props.theme.red};
 `;
+const Input = styled(motion.input)`
+  transform-origin: right center; //변화가 시작하는 위치를 의미
+  position: absolute;
+  left: -150px;
+`;
 
 const logoVariants = {
   normal: {
@@ -79,8 +88,10 @@ const logoVariants = {
 };
 
 function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch("/");
   const tvMatch = useMatch("/tv");
+  const toggleSearch = () => setSearchOpen(prev => !prev);
 
   return (
     <Nav>
@@ -109,7 +120,10 @@ function Header() {
       </Col>
       <Col>
         <Search>
-          <svg
+          <motion.svg
+            onClick={toggleSearch}
+            animate={{ x: searchOpen ? -180 : 0 }}
+            transition={{ type: "linear" }}
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -119,7 +133,12 @@ function Header() {
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
             ></path>
-          </svg>
+          </motion.svg>
+          <Input
+            animate={{ scaleX: searchOpen ? 1 : 0 }}
+            transition={{ type: "linear" }}
+            placeholder="Search for movie or tv show..."
+          />
         </Search>
       </Col>
     </Nav>
